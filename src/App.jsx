aea758dcce97b8282293e4848139d168a0d2d1d9
NorbertG <norbertguzy@o2.pl>
2023-05-31 14:16:00 +0200

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,44 @@ function App() {
   return (
     <div>
       <Navbar />
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe,
+        aperiam. Reprehenderit enim dolorem ullam praesentium saepe accusamus
+        illum explicabo odit, quam, quaerat cum nihil eos commodi esse omnis
+        sit. Est recusandae libero sint dolor odio voluptatum eum quasi maiores
+        aperiam! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Provident harum libero, eveniet, error corporis tempore aut voluptate
+        voluptatem aliquid, cupiditate nesciunt optio rerum quasi totam ratione
+        porro delectus fugit distinctio quaerat sit repudiandae ducimus
+        consequuntur? Tempora praesentium perferendis dolores dicta? Lorem ipsum
+        dolor sit, amet consectetur adipisicing elit. Saepe, aperiam.
+        Reprehenderit enim dolorem ullam praesentium saepe accusamus illum
+        explicabo odit, quam, quaerat cum nihil eos commodi esse omnis sit. Est
+        recusandae libero sint dolor odio voluptatum eum quasi maiores aperiam!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+        harum libero, eveniet, error corporis tempore aut voluptate voluptatem
+        aliquid, cupiditate nesciunt optio rerum quasi totam ratione porro
+        delectus fugit distinctio quaerat sit repudiandae ducimus consequuntur?
+        Tempora praesentium perferendis dolores dicta? Lorem ipsum dolor sit,
+        amet consectetur adipisicing elit. Saepe, aperiam. Reprehenderit enim
+        dolorem ullam praesentium saepe accusamus illum explicabo odit, quam,
+        quaerat cum nihil eos commodi esse omnis sit. Est recusandae libero sint
+        dolor odio voluptatum eum quasi maiores aperiam! Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Provident harum libero, eveniet,
+        error corporis tempore aut voluptate voluptatem aliquid, cupiditate
+        nesciunt optio rerum quasi totam ratione porro delectus fugit distinctio
+        quaerat sit repudiandae ducimus consequuntur? Tempora praesentium
+        perferendis dolores dicta? Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Saepe, aperiam. Reprehenderit enim dolorem ullam
+        praesentium saepe accusamus illum explicabo odit, quam, quaerat cum
+        nihil eos commodi esse omnis sit. Est recusandae libero sint dolor odio
+        voluptatum eum quasi maiores aperiam! Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Provident harum libero, eveniet, error
+        corporis tempore aut voluptate voluptatem aliquid, cupiditate nesciunt
+        optio rerum quasi totam ratione porro delectus fugit distinctio quaerat
+        sit repudiandae ducimus consequuntur? Tempora praesentium perferendis
+        dolores dicta?
+      </h1>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
 import { Link, isRouteErrorResponse } from "react-router-dom";
 
 function Navbar() {
   const [active, setActive] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Joe Doe",
+    isSeller: true,
+  };
+
   return (
     <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
@@ -16,15 +35,35 @@ function Navbar() {
         </div>
         <div className="links">
           <span>Przepisy</span>
-          <button>Login</button>
+          {!currentUser && <span>Korzyści dołączenia</span>}
+          {!currentUser && <button>Login</button>}
+          {currentUser && (
+            <div className="user">
+              <img
+                src="https://images.unsplash.com/photo-1633412802994-5c058f151b66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80"
+                alt="logo"
+              ></img>
+              <span>{currentUser?.username}</span>
+              <div className="options">
+                <span>Ulubione</span>
+                <span>Zamówienia</span>
+                <span>Wiadomości</span>
+                <span>Wyloguj</span>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      <hr></hr>
-      <div className="menu">
-        <span>Test</span>
-        <span>Test</span>
-        <span>Test</span>
-      </div>
+      {active && (
+        <>
+          <hr></hr>
+          <div className="menu">
+            <span>Test</span>
+            <span>Test</span>
+            <span>Test</span>
+          </div>
+        </>
+      )}
     </div>
   );
 }
